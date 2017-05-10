0b6af5938810ba45377f31538872426f92010f8a
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-05-10 23:00:10 +0800

    Update init scripts: remove test tickets & add one more test user

--- init_db.py
+++ init_db.py
@@ -9,11 +9,13 @@ import hashlib
 DB_NAME = 'awesome_tickets'
 USER = 'root'
 PSWD = '123456'
-GEN_SOLD_SEATS = True
+GEN_RAND_SOLD_SEATS = False
+GEN_FIXED_SOLD_SEATS = True
 TEST_SHOW_DATE = ['2017-05-01', '2017-05-02', '2017-05-03']
 TEST_SHOW_TIME = ['10:05:00', '13:20:00', '16:35:00', '19:50:00', '22:05:00']
 TEST_PRICE = [20.5, 22.5, 28, 35, 37, 41.5]
-TEST_PHONE = '18812345678'
+TEST_PHONE_1 = '18812345678'
+TEST_PHONE_2 = '18813572648'
 TEST_TICKET_CODE = "1000000000"
 
 os.system("mysql -u%s -p%s < ./sql/create_db.sql"
@@ -98,8 +100,11 @@ try:
             for i, row in enumerate(seat_rows):
                 col = 1
                 for char in row:
-                    if GEN_SOLD_SEATS:
+                    if GEN_RAND_SOLD_SEATS:
                         available = random.randrange(5)
+                    elif (GEN_FIXED_SOLD_SEATS and movie_on_show_id == 1 and
+                          i == 0 and col == 4):
+                        available = 0
                     else:
                         available = 1
                     if (char != '0'):
@@ -110,33 +115,14 @@ try:
                         """ % (movie_on_show_id, i + 1, col, available))
                         col += 1
 
-        # Add test user
-        print("Adding test user...")
+        # Add test users
+        print("Adding test users...")
         cursor.execute("""
             INSERT INTO User (phone_num) VALUES (%s)
-        """ % TEST_PHONE)
-        cursor.execute("SELECT LAST_INSERT_ID()")
-        user_id = cursor.fetchall()[0][0]
-        print("user_id: %d" % user_id)
-
-        # Add test ticket
-        print("Adding test ticket...")
-        digest = hashlib.md5(TEST_TICKET_CODE.encode("utf-8")).hexdigest()
+        """ % TEST_PHONE_1)
         cursor.execute("""
-            INSERT INTO Ticket (user_id, digest) VALUES (%d, '%s')
-        """ % (user_id, digest))
-        cursor.execute("SELECT LAST_INSERT_ID()")
-        ticket_id = cursor.fetchall()[0][0]
-        print("ticket_id: %d" % ticket_id)
-        cursor.execute("""
-            INSERT INTO TicketCode (code) VALUES ('%s')
-        """ % TEST_TICKET_CODE)
-
-        # Bind ticket to seats
-        print("Binding ticket to seats...")
-        cursor.execute("""
-            UPDATE Seat SET ticket_id=%d WHERE seat_id in (1, 2, 3)
-        """ % ticket_id)
+            INSERT INTO User (phone_num, remain_purchase) VALUES (%s, %s)
+        """ % (TEST_PHONE_2, 0))
 
     conn.commit()
     print("Done.")
